998a6629ee779cd6933ec8c3ad86d9e25a7fa88e
Muditha Dissanayake <mudithaa@gmail.com>
2021-06-29 20:49:04 +1000

    initial commit

---
+++ SignalRFunctionApp/SignalRFunction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace SignalRFunctionApp
+{
+    public static class SignalRFunction
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "mudischat")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
